25f47df3f6e3e08f3ab8ae8a0206eff2421961f6
Muhammad Umar <muhammadumar.tudortmund@gmail.com>
2024-02-11 15:05:50 +0500

    Make operate function and call the operation function according to each case

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ let multiply = function(a,b) {
 let divide = function(a,b) {
 	return a / b;
 };
+
+let firstNum;
+let operator;
+let secondNum;
+
+let operation = function(firstNum,secondNum,operator){
+    switch (operator){
+        case '+':
+            return add(firstNum,secondNum);
+        case '-':
+            return subtract(firstNum,secondNum);
+        case '*':
+            return multiply(firstNum,secondNum);
+        case '/':
+            return divide(firstNum,secondNum);
+        default:
+            return 'Invalid operator';
+    }
+}
