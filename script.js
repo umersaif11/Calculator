e9b78f54b3fc7082759bd5f66c3c1ce849db39ca
Muhammad Umar <muhammadumar.tudortmund@gmail.com>
2024-02-11 15:02:33 +0500

    Make functions of add,subtract,multiply and divide

---
+++ script.js
@@ -0,0 +1,15 @@
+let add = function(a,b) {
+	return a+b;
+};
+
+let subtract = function(a,b) {
+	return a - b;
+};
+
+let multiply = function(a,b) {
+    return a * b;
+};
+
+let divide = function(a,b) {
+	return a / b;
+};
